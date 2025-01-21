0d741bd440ec69a54579de2146a37902d613ecaf
Cyrus Tadjiki <tadjiki@npcresearch.com>
2025-01-21 10:06:56 -0800

    added state_heat_map function

---
+++ R/state_heat_map.R
@@ -0,0 +1,50 @@
+state_heat_map = function(state_data,legend_title = NULL){
+  #' Function for Creating Heat Map of US States
+  #'
+  #' @param data A class of data.frame that has variables state_abbr and count
+  #' @param legend_title A class of "string" that is what you want the legend title to display.
+  #'
+  #' @return ggplot heat map
+  #' @export
+  #'
+  #' @examples
+  #'
+  #'library(usmap)
+  #'
+  #'data = haven::read_sav("P:/6. Projects Active/Harm Reduction in Tx Cts/Survey/Data/Alchemer Exports/Harm Reduction Survey_working.sav") %>%
+  #' sjlabelled::as_label() %>%
+  #'   select(state_abbr = Q1.3_4, type = Q1.1) %>%
+  #'   # filter(type == "Adult Treatment Court") %>%
+  #'   group_by(state_abbr) %>%
+  #'   summarise(count = n())
+  #'
+  #'state_heat_map(data)
+  #'
+
+  library(usmap)
+
+  if(is.null(legend_title)) legend_title = "Number of programs"
+
+  # Join state abbreviations to full state names
+  state_data <- left_join(state_data, state_abbrev, by = c("state_abbr" = "abbr"))
+
+  # Get map data for US states
+  states_map <- map_data("state")
+
+  # Joining summarized data with the map data
+  map_data <- left_join(states_map, state_data, by = c("region" = "state"))
+
+  # Plot the density map
+  ggplot(map_data, aes(x = long, y = lat, group = group, fill = count)) +
+    geom_polygon(color = "black") +  # Draw state boundaries
+    scale_fill_continuous(name = legend_title,
+                          low = "lightgray", high = "#305496",
+                          na.value = "white", guide = "colorbar") +  # Gray for missing states
+    theme_minimal() +
+    theme(axis.title.x = element_blank(),  # Remove axis labels for cleaner map
+          axis.title.y = element_blank(),
+          axis.text = element_blank(),
+          axis.ticks = element_blank(),
+          panel.grid = element_blank())
+
+}
